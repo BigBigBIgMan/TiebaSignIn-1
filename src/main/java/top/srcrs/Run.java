1451c6e2bed67c77cdaff0cb83b3b7d9a2ccd0c6
BigBigBIgMan <1741999340@qq.com>
2023-08-31 11:50:15 +0800

    Update Run.java

--- src/main/java/top/srcrs/Run.java
+++ src/main/java/top/srcrs/Run.java
@@ -14,8 +14,8 @@ public class Run {
         System.out.println("有道云签到耗时："+timer.intervalRestart());
         String notice = AliYunDrive.aliYunDriveCheckin();
         System.out.println("阿里云盘签到耗时："+timer.intervalRestart());
-        //String content = YouDaoNote.content;
-        //TellMsg.tellMsg(title, content +notice);
+        String content = YouDaoNote.content;
+        TellMsg.tellMsg(title, content +notice);
         System.out.println("消息推送耗时："+timer.intervalRestart());
     }
 }
