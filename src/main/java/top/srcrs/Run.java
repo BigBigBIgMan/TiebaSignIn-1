71770f478271741a3ba8ceb1f2befa2992a04df4
BigBigBIgMan <1741999340@qq.com>
2023-08-31 12:00:01 +0800

    Update Run.java

--- src/main/java/top/srcrs/Run.java
+++ src/main/java/top/srcrs/Run.java
@@ -10,12 +10,12 @@ public class Run {
 
     public static void main(String[] args) throws UnknownHostException {
         TimeInterval timer = DateUtil.timer();
-        String title = YouDaoNote.YouDaoNoteDo();
+        //String title = YouDaoNote.YouDaoNoteDo();
         System.out.println("有道云签到耗时："+timer.intervalRestart());
         String notice = AliYunDrive.aliYunDriveCheckin();
         System.out.println("阿里云盘签到耗时："+timer.intervalRestart());
-        String content = YouDaoNote.content;
-        TellMsg.tellMsg(title, content +notice);
+        //String content = YouDaoNote.content;
+        //TellMsg.tellMsg(title, content +notice);
         System.out.println("消息推送耗时："+timer.intervalRestart());
     }
 }
