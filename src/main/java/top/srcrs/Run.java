d3c7c23b23c6170f2ff8078cd8ac36248e8864d6
BigBigBIgMan <1741999340@qq.com>
2023-08-31 10:34:45 +0800

    Update Run.java

--- src/main/java/top/srcrs/Run.java
+++ src/main/java/top/srcrs/Run.java
@@ -74,29 +74,30 @@ public class Run {
     {
         String code = "";
         try {
-            String purl  = "http://www.pushplus.plus/send?token=ebbee855fc8942708aa6f50c9e9cd66d&title="+title+"&content="+msg;
-            HttpResponse responseP = HttpRequest.post(purl).execute();
-            String responsePStr = responseP.body();
-            JSONObject responsePJson = JSONUtil.parseObj(responsePStr);
-             code = responsePJson.getStr("code");
-            System.out.println(responsePStr);
+            String url = "https://sctapi.ftqq.com/SCT207695TV2Mu3zwYCq2JN8dS1sGrSIBF.send?title="+title+"&desp="+msg;
+            HttpRequest.setGlobalTimeout(2000);
+            HttpResponse response  = HttpRequest.post(url).execute();
+            String responseStr = response.body();
+            JSONObject responseJson = JSONUtil.parseObj(responseStr);
+            code = responseJson.getStr("code");
+            System.out.println(responseStr);
+//            {"code":0,"message":"","data":{"pushid":"136742982","readkey":"SCTU5pmCJ9UyGvh","error":"SUCCESS","errno":0}}
         } catch (Exception e) {
-           e.printStackTrace();
+            String message = e.getMessage();
+            System.out.println(message);
         }
 
-        if(!"200".equals(code)){
-            String url = "https://sctapi.ftqq.com/SCT207695TV2Mu3zwYCq2JN8dS1sGrSIBF.send?title="+title+"&desp="+msg;
+        if(!"0".equals(code)){
             try {
-                HttpRequest.setGlobalTimeout(2000);
-                HttpResponse response  = HttpRequest.post(url).execute();
-                String responseStr = response.body();
-                System.out.println(responseStr);
+                String purl  = "http://www.pushplus.plus/send?token=ebbee855fc8942708aa6f50c9e9cd66d&title="+title+"&content="+msg;
+                HttpResponse responseP = HttpRequest.post(purl).execute();
+                String responsePStr = responseP.body();
+//                JSONObject responsePJson = JSONUtil.parseObj(responsePStr);
+//                code = responsePJson.getStr("code");
+                System.out.println(responsePStr);
             } catch (Exception e) {
-                String message = e.getMessage();
-                System.out.println(message);
+                e.printStackTrace();
             }
-//            System.out.println(responseStr);
-            //        {"code":0,"message":"","data":{"pushid":"136742982","readkey":"SCTU5pmCJ9UyGvh","error":"SUCCESS","errno":0}}
         }
     }
 }
