484a4514364e11312bf61eb462721186ea98a1bc
BigBigBIgMan <1741999340@qq.com>
2024-03-15 15:08:05 +0800

    Update Run.java
    Signed-off-by: BigBigBIgMan <1741999340@qq.com>

--- src/main/java/top/srcrs/Run.java
+++ src/main/java/top/srcrs/Run.java
@@ -12,14 +12,16 @@ public class Run {
         TimeInterval timer = DateUtil.timer();
         String title = YouDaoNote.YouDaoNoteDo();
         System.out.println("有道云签到耗时："+timer.intervalRestart());
+
         String notice = AliYunDrive.aliYunDriveCheckin();
         System.out.println(notice);
         System.out.println("阿里云盘签到耗时："+timer.intervalRestart());
         String pcr532Msg = Pcr532.checkIn();
         System.out.println("PCR532推送耗时："+timer.intervalRestart());
-        System.out.println("PCR532返回内容："+pcr532Msg);
+        String tradePage = YKT.tradePage();
+        System.out.println("有看投："+timer.intervalRestart());
         String content = YouDaoNote.content;
-        TellMsg.tellMsg(title, content +notice+"%0D%0A%0D%0A "+pcr532Msg);
+        TellMsg.tellMsg(title, content +notice+"%0D%0A%0D%0A "+pcr532Msg+"%0D%0A%0D%0A "+tradePage);
         System.out.println("消息推送耗时："+timer.intervalRestart());
 
     }
